97562a4d11bae0094f6710a2e1455420313ec845
dungngoc99 <dung.trinhngoc.tnd@gmail.com>
2022-12-31 20:45:54 +0700

    update homework

--- BC42_JS4_Trịnh Ngọc Dung (2)/.vscode/BT4_2.js
+++ BC42_JS4_Trịnh Ngọc Dung (2)/.vscode/BT4_2.js
@@ -1,10 +1,41 @@
 // Bài 1
-let day = +document.getElementById("day").value;
-let month = +document.getElementById("month").value;
-let year = +document.getElementById("year").value;
-const month1 = Array(1, 3, 5, 7, 8, 10, 12);
-document.getElementById("ex1").onclick = function () {
-  if ((month = month1)) {
-    day;
+
+function ex1() {
+  let day = +document.getElementById("day").value;
+  let month = +document.getElementById("month").value;
+  let year = +document.getElementById("year").value;
+  if (day === 31 && month === 12) {
+    day = 1;
+    month = 1;
+    year++;
+  } else if (month === 2) {
+    if (day === 28) {
+      day = 1;
+      month++;
+    } else {
+      day++;
+    }
+  } else if (
+    month === 1 ||
+    month === 3 ||
+    month === 5 ||
+    month === 7 ||
+    month === 8 ||
+    month === 10 ||
+    month === 12
+  ) {
+    if (day === 31) {
+      day = 1;
+      month++;
+    } else {
+      day++;
+    }
+  } else {
+    if (day === 31) {
+      day = 1;
+      month++;
+    } else day++;
   }
-};
+  let ex1El = document.getElementById("result1");
+  ex1El.innerHTML = `Mai là ngày ${day} tháng ${month} năm ${year}`;
+}
