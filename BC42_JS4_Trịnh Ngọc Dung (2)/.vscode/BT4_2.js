562c0fe8aa2a7101c4a533db5b8b7f6f8f8edbc0
dungngoc99 <dung.trinhngoc.tnd@gmail.com>
2022-12-31 20:18:17 +0700

    update homework

---
+++ BC42_JS4_Trịnh Ngọc Dung (2)/.vscode/BT4_2.js
@@ -0,0 +1,10 @@
+// Bài 1
+let day = +document.getElementById("day").value;
+let month = +document.getElementById("month").value;
+let year = +document.getElementById("year").value;
+const month1 = Array(1, 3, 5, 7, 8, 10, 12);
+document.getElementById("ex1").onclick = function () {
+  if ((month = month1)) {
+    day;
+  }
+};
