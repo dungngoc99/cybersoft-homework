19240c223fb42aa2c4c36ff8c67a2c2cbf06d079
dungngoc99 <dung.trinhngoc.tnd@gmail.com>
2023-01-13 21:06:23 +0700

    update

--- BC42_JS6_Trịnh Ngọc Dung(2)/BT6_2.js
+++ BC42_JS6_Trịnh Ngọc Dung(2)/BT6_2.js
@@ -1,69 +1,37 @@
-//Input:Thu nhập cá nhân, số người phụ thuộc, họ và tên người dùng
-//Logic:
-//- Dom đến giá trị mà người dùng nhập vào rồi gọi tên cho chúng
-//- Sử dụng hàm if để chia ra các trường hợp, sau đó tính toán theo các mức giá đã được đề ra
-//Output: Xuất ra số tiền
-function result1() {
-  const perIn = +document.getElementById("perIn").value;
-  const staff = +document.getElementById("staff").value;
-  let vat = perIn - 4e6 - staff * 1.6e6;
-  if (vat <= 60e6) {
-    vat *= 0.05;
-  } else if (vat > 60e6 && vat <= 120e6) {
-    vat *= 0.1;
-  } else if (vat > 120e6 && vat <= 210e6) {
-    vat *= 0.15;
-  } else if (vat > 210e6 && vat <= 384e6) {
-    vat *= 0.2;
-  } else if (vat > 384e6 && vat <= 624e6) {
-    vat *= 0.25;
-  } else if (vat > 624e6 && vat <= 960e6) {
-    vat *= 0.3;
+function kiem_tra_snt(n) {
+  // Biến cờ hiệu
+  var flag = true;
+  // Nếu n bé hơn 2 tức là không phải số nguyên tố
+  if (n < 2) {
+    flag = false;
+  } else if (n == 2) {
+    flag = true;
+  } else if (n % 2 == 0) {
+    flag = false;
   } else {
-    vat *= 0.35;
-  }
-  const ex1 = document.getElementById("ex1");
-  ex1.innerHTML = `Thuế thu nhập cá nhân là : ${vat} VNĐ`;
-}
-// Bài 2:
-//Input:loại khách hàng, mã khách hàng, số kênh cao cấp
-//Logic:
-//- Dom đến giá trị mà người dùng nhập vào rồi gọi tên cho chúng
-//- Sử dụng addEventListener để thêm bớt nội dung hiển thị trên màn hình
-//- Sử dụng hàm if để chia ra các trường hợp, sau đó tính toán theo các mức giá đã được đề ra
-//Output: Xuất ra số tiền
-const form = document.getElementById("form");
-const user = document.getElementById("user");
-const ex2 = document.getElementById("ex2");
-
-user.addEventListener("change", function (event) {
-  const typeCustomer = event.target.value;
-  if (typeCustomer === "company") {
-    const phoneNumberNode = document.createElement("input");
-    phoneNumberNode.setAttribute("placeholder", "Số kết nối");
-    phoneNumberNode.setAttribute("type", "text");
-    phoneNumberNode.setAttribute("id", "phoneNumber");
-    form.appendChild(phoneNumberNode);
-  } else {
-    const phoneNumberNode = document.getElementById("phoneNumber");
-    form.removeChild(phoneNumberNode);
-  }
-});
-
-function result2() {
-  let phoneNumber = document.getElementById("phoneNumber");
-  const phoneNumberValue = +phoneNumber.value;
-  let sum = 0;
-  const chanelId = document.getElementById("chanelId").value;
-  if (user === "company") {
-    if (phoneNumberValue <= 10) {
-      sum = 15 + 7.5 * phoneNumberValue;
-    } else {
-      sum = 75 + (phoneNumberValue - 10) * 5;
+    // lặp từ 3 tới n-1 với bước nhảy là 2 (i+=2)
+    for (var i = 3; i <= Math.sqrt(n); i += 2) {
+      if (n % i == 0) {
+        flag = false;
+        break;
+      }
     }
-  } else {
-    sum = 4.5 + 20.5 + chanelId * 7.5;
   }
-  console.log("sum:", sum);
-  ex2.innerHTML = `Số tiền cáp là: ${sum}$`;
+  return flag;
+}
+
+// Hàm in ra các số nguyên tố từ 1 tới n
+function print() {
+  // Lấy number
+  var number = document.getElementById("number").value;
+
+  // Lặp để in kết quả
+  var html = "";
+  for (var i = 1; i <= number; i++) {
+    // Nếu là số nguyên tố thì in ra
+    if (kiem_tra_snt(i) === true) {
+      html += i + "&nbsp";
+    }
+  }
+  document.getElementById("result").innerHTML = html;
 }
